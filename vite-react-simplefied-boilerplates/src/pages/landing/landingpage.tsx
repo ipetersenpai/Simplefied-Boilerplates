0224d246b6df902793f79e1e2004bf1767d99a13
Peter Francis Robante <robantepeterfran@gmail.com>
2024-05-18 21:00:22 +0800

    feat:initial react-vite boilerplate

---
+++ vite-react-simplefied-boilerplates/src/pages/landing/landingpage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <div>
+      <p>landing page</p>
+    </div>
+  )
+}
+
+export default Landingpage
---
+++ vite-react-simplefied-boilerplates/src/routes/protected.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+const Protected = () => {
+  return (
+    <div>
+            <p>undefined</p>
+    </div>
+  )
+}
+
+export default Protected
